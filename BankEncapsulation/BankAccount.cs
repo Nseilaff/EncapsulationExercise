ebd38705675f18ad283221bbd753a6bb50be529a
Nate S <seilaffn@gmail.com>
2022-08-19 10:41:57 -0600

    First commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount(double amount)
+        {
+            balance = amount;  
+        }
+        private double balance = 0;
+        
+        public static double Deposit()
+        {
+
+            Console.WriteLine("Please enter amount to deposit: ");
+            bool deposit = double.TryParse(Console.ReadLine(), out double newDeposit);
+
+            while(deposit == false)
+            {
+                Console.WriteLine("Please try again");
+                break;
+            }
+            return newDeposit;
+         }
+
+       
+        public double GetBalance()
+        {
+
+            return balance;
+            
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankEncapsulation;
+
+Console.WriteLine("How much would you like to deposit");
+var amount = double.Parse(Console.ReadLine());
+var account = new BankAccount(amount);
+Console.WriteLine($"Your new Balance is: {account.GetBalance()}");
